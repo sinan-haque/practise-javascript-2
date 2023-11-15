3d617adf9d5f996e0ad4c5640e20857009bbfad8
sinan-haque <sinanhaque10@gmail.com>
2023-11-15 22:05:13 +0600

    add

--- file1.js
+++ file1.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+
+console.log(this);
+console.log(window);
+console.log(fname);
+var fname='sinan haque'
+console.log(fname);
+
+
+console.log(fname2)
+let fname2='sinan';
+console.log(fname2)
